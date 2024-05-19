0de49b385518a47f5875f031587e49adc07e66e0
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-05-19 11:20:50 +0530

    Update output.tf

--- modules/security_group/output.tf
+++ modules/security_group/output.tf
@@ -1,7 +1,7 @@
 output "public_security_group_id" {
-    value = aws_security_group.opensearch_publicsg.id
+    value = aws_security_group.redis_publicsg.id
 }
 
 output "private_security_group_id" {
-    value = aws_security_group.opensearch_privatesg.id
+    value = aws_security_group.redis_privatesg.id
 }
