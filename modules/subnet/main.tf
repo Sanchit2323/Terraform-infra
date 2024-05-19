bfa4e2a7086f5e03a1234c0c4f49ae4c4d01ba4e
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-05-19 11:21:35 +0530

    Update main.tf

--- modules/subnet/main.tf
+++ modules/subnet/main.tf
@@ -1,4 +1,4 @@
-resource "aws_subnet" "opensearch_publicsubnet" {
+resource "aws_subnet" "redis_publicsubnet" {
   availability_zone = var.publiczone
   vpc_id = var.vpc_id
   map_public_ip_on_launch = true
@@ -9,7 +9,7 @@ resource "aws_subnet" "opensearch_publicsubnet" {
 }
 
 
-resource "aws_subnet" "opensearch_privatesubnet" {
+resource "aws_subnet" "redis_privatesubnet" {
   availability_zone = "${element(var.privatezones, count.index)}"
   count    = length(var.private_subnets_cidr)
   vpc_id = var.vpc_id
