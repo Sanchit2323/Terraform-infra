520d9fe075788cc059f1162d6c06317e72797b5a
Sanchit2323 <153765874+Sanchit2323@users.noreply.github.com>
2024-05-19 11:22:31 +0530

    Update output.tf

--- modules/subnet/output.tf
+++ modules/subnet/output.tf
@@ -1,7 +1,7 @@
 output "aws_public_subnet_id" {
-    value = aws_subnet.opensearch_publicsubnet.id
+    value = aws_subnet.redis_publicsubnet.id
 }
 
 output "aws_private_subnet_ids" {
-    value = "${aws_subnet.opensearch_privatesubnet.*.id}"
+    value = "${aws_subnet.redis_privatesubnet.*.id}"
 }
